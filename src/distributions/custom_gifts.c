f891f9e8934328f683754c0d5cf522112470be1b
WhenGryphonsFly <84215159+WhenGryphonsFly@users.noreply.github.com>
2021-10-17 19:07:51 -0500

    Update custom_gifts.c

--- src/distributions/custom_gifts.c
+++ src/distributions/custom_gifts.c
@@ -1,6 +1,6 @@
 #ifdef GUARD_CUSTOM_CODE_C
 
-u8 custom_gText_distCg_Ultima[] = _("Ultima");
+const u8 custom_gText_distCg_Ultima[] = _("Ultima");
 const struct MysteryGiftDistributionData custom_mgdd_Ultima = {
 	1, 1,
 	{1008, WC_ICON_UNOWN_QM, 1, WC_TYPE_STANDARD, WC_COLOR_SILVER, WC_SHARE_YES, 1, _("Test 1"), _("Test 2WWWW"), {_("Test 3"), _("Test 4"), _("Test 5"), _("Test 6")}, _("Test 7"), _("Test 8")},
